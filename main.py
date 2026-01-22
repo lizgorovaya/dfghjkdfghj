f56d3340ee72cf480baa5d322910e163f0ea45a6
lizgorovaya <166170126+lizgorovaya@users.noreply.github.com>
2026-01-22 21:42:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+    
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot("8218486122:AAGuTb7VTRix8EDt2cSXd7nX8XvRbQ0T3gg")
+
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+@bot.message_handler(commands=["ping"])
+def on_ping(message):
+    bot.reply_to(message, "Still alive and kicking!")
+
+# Запуск бота
+bot.polling()
